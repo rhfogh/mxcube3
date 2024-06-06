e09f6c51e2b8f5cab890191a785d89063508f6df
Marcus Oskarsson <oscarsso@esrf.fr>
2024-06-06 18:06:56 +0200

    [Queue Progress] - Fixed progress

--- mxcubeweb/core/components/queue.py
+++ mxcubeweb/core/components/queue.py
@@ -2021,7 +2021,6 @@ class Queue(ComponentBase):
         return self.app.AUTO_MOUNT_SAMPLE
 
     def get_task_progress(self, node, pdata):
-        return 0
         progress = 0
 
         if node.is_executed():
